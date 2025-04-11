06923053266a0cb7b898b341bea2f3cd7cedb1d6
Abhishek Lodhi <142293927+Lodhiabhi02@users.noreply.github.com>
2025-04-12 02:32:03 +0530

    Update Navbar.js

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,7 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { FaBars, FaTwitter } from 'react-icons/fa'
 import { links, social } from './data'
-import logo from './logo.svg'
+import logo from './logo.svg' this image is import from logo.svg
+
 
 const Navbar = () => {
   return <h4>navbar</h4>
